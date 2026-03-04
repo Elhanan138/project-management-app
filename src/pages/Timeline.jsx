c1fa39ca2b649e6695d44e083ca79fcf90d7d830
אלחנן חזן <elhanan138@gmail.com>
2026-03-04 11:27:48 +0000

    File changes

--- src/pages/Timeline.jsx
+++ src/pages/Timeline.jsx
@@ -131,7 +131,7 @@ export default function Timeline() {
             <thead className="bg-slate-50 border-b border-slate-200 text-slate-500 text-sm">
               <tr>
                 <th className="p-4 font-medium">שם השלב</th>
-                <th className="p-4 font-medium">לקוח</th>
+                <th className="p-4 font-medium">לקוחות שבשלב זה</th>
                 <th className="p-4 font-medium w-24">פעולות</th>
               </tr>
             </thead>
